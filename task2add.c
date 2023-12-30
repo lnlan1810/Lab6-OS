af3261a215b0741cf6cd03bd81ba1ecca8cadf3b
Le Thi Ngoc Lan <90277811+lnlan1810@users.noreply.github.com>
2023-12-30 11:27:06 +0300

    Create task2add.c

---
+++ task2add.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+
+#define MAX_BYTES 255
+
+int running_processes = 0;
+
+void search_in_file(const char *filename, const char *search_string) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[MAX_BYTES];
+    size_t totalBytesRead = 0;
+    int occurrences = 0;
+
+    while (1) {
+        size_t bytesRead = fread(buffer, 1, sizeof(buffer), file);
+        if (bytesRead == 0) {
+            break;
+        }
+
+        for (size_t i = 0; i < bytesRead; ++i) {
+            if (memcmp(buffer + i, search_string, strlen(search_string)) == 0) {
+                occurrences++;
+            }
+        }
+
+        totalBytesRead += bytesRead;
+    }
+    
+    printf("PID: %d, File: %s, Total Bytes Read: %zu, Occurrences: %d, Running Processes: %d\n",
+           getpid(), filename, totalBytesRead, occurrences, running_processes);
+
+    fclose(file);
+}
+
+void increment_running_processes() {
+    running_processes++;
+}
+
+void decrement_running_processes() {
+    running_processes--;
+}
+
+void search_in_directory(const char *dirname, const char *search_string, int max_processes) {
+    DIR *dir = opendir(dirname);
+    if (dir == NULL) {
+        perror("Error opening directory");
+        exit(EXIT_FAILURE);
+    }
+
+    struct dirent *entry;
+
+    increment_running_processes();
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG) 
+        {
+            char path[PATH_MAX];
+
+            if (snprintf(path, sizeof(path), "%s/%s", dirname, entry->d_name) >= sizeof(path)) {
+                fprintf(stderr, "Path buffer overflow\n");
+                closedir(dir);
+                exit(EXIT_FAILURE);
+            }
+
+            pid_t child_pid = fork();
+
+            if (child_pid == -1) {
+                perror("Error creating child process");
+                exit(EXIT_FAILURE);
+            } else if (child_pid == 0) {
+                search_in_file(path, search_string);
+                exit(EXIT_SUCCESS);
+            }
+        }
+    }
+
+    closedir(dir);
+
+    decrement_running_processes();
+
+    while (running_processes > 0) {
+        int status;
+        wait(&status);
+        if (WIFEXITED(status)) {
+            running_processes--;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <directory_name> <search_string> <max_processes>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char *dirname = argv[1];
+    char *search_string = argv[2];
+    int max_processes = atoi(argv[3]);
+
+    if (max_processes <= 0) {
+        fprintf(stderr, "Invalid value for max_processes\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (opendir(dirname) == NULL) {
+        perror("Error opening directory");
+        exit(EXIT_FAILURE);
+    }
+
+    search_in_directory(dirname, search_string, max_processes);
+
+    return 0;
+}
